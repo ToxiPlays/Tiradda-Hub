7329595aa298be741262a9df7b31d72efc4ac5a6
CryfryDoesGaming <kicksfortricks4@gmail.com>
2019-03-14 15:22:34 -0700

    Modifying h>credit
    Bright Lightning's alt was removed from credit via request.

--- bot.lua
+++ bot.lua
@@ -72,7 +72,7 @@ function CheckForCommands(message, arguments)
 		message.channel:send {
 									  embed = {
 										title = "Thank you to these many people.",
-										description = "Your contributions to the bot have not gone unnoticed.\nToxiPlays#9278 - Bot Owner\nBright Lightning#6416 (and #9532) - Bot Developer\n[FuZion] Sexy Cow#0018 - Buffed h>help",
+										description = "Your contributions to the bot have not gone unnoticed.\nToxiPlays#9278 - Bot Owner\nBright Lightning#6416 - Bot Developer\n[FuZion] Sexy Cow#0018 - Buffed h>help",
 										color = discordia.Color.fromRGB(0, 255, 0).value,
 										timestamp = discordia.Date():toISO('T', 'Z')
 									  }
